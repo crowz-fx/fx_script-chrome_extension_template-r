1250ad799d20610cd7bf3929cd638511fda2ddff
crowz-fx <crowzfx@outlook.com>
2019-12-25 23:37:33 +0000

    finished off, handling passed in args and updates to readme

--- README.md
+++ README.md
@@ -39,23 +39,23 @@ All of the required options and any optional ones you want to specify
 
 #### 1. Script with parameters;
 ```
-./template-r.sh --author="Lui" --ext-name="Template-r" --ext-short-name="plate-r" --description="Create a template"
+./template-r.sh --author="Lui" --name="Template-r" --short="plate-r" --description="Create a template"
 ```
 
 ##### Required parameters
 ```
-# --author="<String>"          ;; default="Joe Bloggs"               ;; name of the author
-# --ext-name="<String>"        ;; default="Amazing Extension"        ;; name of the extension
-# --ext-short-name="<String>"  ;; default="Shorty"                   ;; short name of the extension
-# --description="<String>"     ;; default="What a great extension"   ;; no explanation needed
+# --author="<String>"      ;; default="Joe Bloggs"               ;; name of the author
+# --name="<String>"        ;; default="Amazing Extension"        ;; name of the extension
+# --short="<String>"       ;; default="Shorty"                   ;; short name of the extension
+# --desc="<String>"        ;; default="What a great extension"   ;; no explanation needed
 ```
 
 ##### Optional parameters
 ```
-# --output-location="<String>" ;; default=cwd                        ;; where to output the output folder
-# --ext-permissions="<String>" ;; default=""                         ;; permissions to give the app i.e. tabs
-# --git-init                   ;; default=false                      ;; whether to git init 
-# --git-commit-msg="<String>"  ;; default="Initial commit and setup" ;; message for the intial commit message (have to specified the git-init parameter)
+# --output="<String>"      ;; default=cwd                        ;; where to output the output folder
+# --permissions="<String>" ;; default=""                         ;; permissions to give the app i.e. tabs
+# --gitinit=true           ;; default=false                      ;; whether to git init and start git source control
+# --commitmsg="<String>"   ;; default="Initial commit and setup" ;; message for the intial commit message (have to specified the git-init parameter)
 ```
 
 #### 2. Run script interactively
--- template-r.sh
+++ template-r.sh
@@ -19,11 +19,16 @@ git_commit_msg="Initial commit and setup"
 # -----
 interactive=false
 full_path_to_package=""
-folder_name=
+folder_name=""
+# this is used in the check as bash doesn't know how to pass back Strings
+global_arg_value=""
 
 # $@ - all args passed in
 handle_input() {
-    echo ""
+    # ensure the args are split into an array
+    IFS="--" read -r -a passed_in_args <<< "$@"
+    unset IFS
+
     if [ "$#" -eq 0 ]; then
         interactive=true
         echo "Interactive mode activated..."
@@ -59,17 +64,95 @@ handle_input() {
     elif [ "$#" -gt 1 ]; then
         echo "Using parameters passed in..."
 
-        # TO-DO
-        for input in "$@"
-        do
-            echo $input
-        done
+        if [ "$#" -lt 4 ]; then
+            error_out_displaying_usage
+        fi
+
+        check_for_arg "author"
+        if [ $? -eq 0 ]; then
+            echo ""
+            echo "No author value has been supplied!"
+            error_out_displaying_usage
+        else
+            ext_author=$global_arg_value
+        fi
+
+        check_for_arg "name"
+        if [ $? -eq 0 ]; then
+            echo ""
+            echo "No name for the extension has been supplied!"
+            error_out_displaying_usage
+        else
+            ext_name=$global_arg_value
+        fi
+
+        check_for_arg "short"
+        if [ $? -eq 0 ]; then
+            echo ""
+            echo "No short name for the extension has been supplied!"
+            error_out_displaying_usage
+        else
+            ext_short_name=$global_arg_value
+        fi
+
+        check_for_arg "desc"
+        if [ $? -eq 0 ]; then
+            echo ""
+            echo "No description for the extension has been supplied!"
+            error_out_displaying_usage
+        else
+            ext_description=$global_arg_value
+        fi
+
+        # optional params, if they exist then set them
+        check_for_arg "output"
+        if [ $? -eq 1 ]; then
+            output_location_dir=`echo "$global_arg_value" | awk '{$1=$1};1'` # hack to remove trailing space
+        fi
+
+        check_for_arg "permissions"
+        if [ $? -eq 1 ]; then
+            ext_permissions=$global_arg_value
+        fi
+
+        check_for_arg "permissions"
+        if [ $? -eq 1 ]; then
+            ext_permissions=$global_arg_value
+        fi
+
+        check_for_arg "gitinit"
+        if [ $? -eq 1 ]; then
+            git_init=true
+        fi
+
+        check_for_arg "commitmsg"
+        if [ $? -eq 1 ]; then
+            git_commit_msg=$global_arg_value
+        fi
     else 
         error_out_displaying_usage
     fi
 
     folder_name=$(echo $(echo "${ext_name}" | tr "[:upper:]" "[:lower:]") | tr "[ ]" "[_]")
     full_path_to_package="${output_location_dir}/${folder_name}"
+}
+
+check_for_arg() {
+    arg_to_check=$1
+    for i in "${passed_in_args[@]}"
+    do
+        temp_index_name=`echo "${i}" | cut -d "=" -f 1`
+        if [[ $temp_index_name == $arg_to_check ]]; then
+            global_arg_value=`echo "${i}" | cut -d "=" -f 2`
+            if [[ $global_arg_value == "" ]] || [[ $global_arg_value == $arg_to_check ]]; then
+                return 0
+            else
+                return 1
+            fi
+        fi
+    done
+
+    return 0
 }
 
 error_out_displaying_usage() {
@@ -108,14 +191,27 @@ echo_out_final_output() {
 create_package() {
     echo ""
     echo "Creating package..."
-    if [ -d $full_path_to_package ]; then
+    if [ -d "${full_path_to_package}" ]; then
         echo "Folder to create the package in already exists, exiting..."
         echo ""
         exit 1
     fi
 
     mkdir -p $full_path_to_package
+    if [ $? -gt 0 ]; then
+        echo ""
+        echo "Failed to make directory, exiting..."
+        echo ""
+        exit 1
+    fi
+
     cp -R "`pwd`/base_template/" $full_path_to_package
+    if [ $? -gt 0 ]; then
+        echo ""
+        echo "Failed to copy templating files, exiting..."
+        echo ""
+        exit 1
+    fi
     
     find ${full_path_to_package} -type f -exec sed -i '' "s/%%TEMPLATE_NAME%%/${ext_name}/g" {} +
     find ${full_path_to_package} -type f -exec sed -i '' "s/%%TEMPLATE_SHORT_NAME%%/${ext_short_name}/g" {} +
@@ -133,6 +229,8 @@ create_package() {
 
         echo "--> Adding in inital commit..."
         git commit -m "${git_commit_msg}"
+
+        cd ${current_dir}
     fi
 }
 
